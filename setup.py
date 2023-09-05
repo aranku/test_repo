bb00e044334e72734bb11a3947dad0bb28c3b1d5
Arun A. Kumar <arun.anthony.kumar@gmail.com>
2023-09-04 17:37:59 -0700

    test again

--- setup.py
+++ setup.py
@@ -5,12 +5,8 @@ print(torch.__version__)
 from setuptools import setup, find_packages
 raise Exception("wtf")
 
-setup(name='funniest',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
-      zip_safe=False)
+setup(
+    name="funniest",
+    version="0.1",
+    packages=find_packages(),
+)
